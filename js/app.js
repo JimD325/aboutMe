3944eed9adab0de854c8aa6e21e907c1dd442927
Jim Doyle <jamesdoyle202@gmail.com>
2021-12-12 18:08:06 -0800

    finalized lab 3

--- js/app.js
+++ js/app.js
@@ -1,88 +1,106 @@
-'use strict'
+'use strict';
 
 
-let username =prompt( 'Hello! my name is Jim, what is your name?' ); // getting their name 
+let username =prompt( 'Hello! my name is Jim, what is your name?' ); // getting their name
 //console.log(username)
 alert ('Hello ' + username + ' it is a pleasure to meet you!');
 
 let congrats = 'correct!';
-let wrong = 'incorrect =(';      // right/wrong responses to questions
+let wrong = 'incorrect =('; // right/wrong responses to questions
+let totalRight = 0;
 
-
-let dogs = prompt('Now we can start with some basic facts about me. Yes or no, do you think I like dogs?').toLowerCase(); // using prompt and toLowerCase methods to remove case sensitivity for users input to the question. 
+let dogs = prompt('Now we can start with some basic facts about me. Yes or no, do you think I like dogs?').toLowerCase(); // using prompt and toLowerCase methods to remove case sensitivity for users input to the question.
 //console.log(dogs)
 
 if (dogs === 'yes' || dogs === 'y') {
 //console.log(congrats)
   alert(congrats);
+  totalRight = totalRight +1;
 }
 else {
 //console.log(wrong)
   alert(wrong);
 }
-let bbq = prompt('Do I enjoy barbeque?').toLowerCase()
+
+let bbq = prompt('Do I enjoy barbeque?').toLowerCase();
 if (bbq === 'yes' || bbq === 'y') {
-alert(congrats)
+  alert(congrats);
+  totalRight = totalRight +1;
 }
 else {
-alert(wrong)
+  alert(wrong);
 }
 //console.log(bbq)
-let vet = prompt('Am I a veteran?').toLowerCase()
+let vet = prompt('Am I a veteran?').toLowerCase();
 if (vet === 'yes' || vet === 'y'){
-alert(congrats)
+  alert(congrats);
+  totalRight = totalRight +1;
 }
 else {
-alert(wrong)
+  alert(wrong);
 }
 //console.log(vet)
-let choc = prompt('Do I like chocolate?').toLowerCase()
+let choc = prompt('Do I like chocolate?').toLowerCase();
 if(choc === 'no' || choc === 'n'){
-alert (congrats)
+  alert (congrats);
+  totalRight = totalRight +1;
 }
 else {
-alert (wrong)
+  alert (wrong);
 }
 //console.log(choc)
-let hike = prompt('Do I like to hike?').toLowerCase()
+let hike = prompt('Do I like to hike?').toLowerCase();
 if (hike === 'yes' || hike === 'y'){
-alert (congrats)
+  alert (congrats);
+  totalRight = totalRight +1;
 }
 else {
-alert (wrong)
+  alert (wrong);
 }
-let attemptnumber = 0; 
-let correctnum = 4
+
 
 
 for ( let i = 0; i < 4; i++){
-let numguess = prompt('What number am I thinking of between 1 and 10?')
-if (numguess >= 5){
-alert ('too high!');
+  let numguess = prompt('What number am I thinking of between 1 and 10?');
+  if (numguess >= 5){
+    alert ('too high!');
+  }
+  else if (numguess <= 3){
+    alert ('too low!');
+  }
+  else if (numguess == 4){
+    alert ('correct!');
+    totalRight = totalRight +1;
+    break;
+  }
+  if (i === 3){
+    alert ('I was thinking of four, we can get it next time');
+  }
 }
-    else if (numguess <= 3){
-        alert ('too low!')
-    }
-    else if (numguess == 4){
-        alert ('correct!')
-        break;
-    }
-    if (i === 3){
-        alert ('I was thinking of four, we can get it next time')
-    }
+let favFoods = ['cookies', 'steak', 'bacon']; //creating foods array
+for ( let i = 0; i < 6; i++){ //for loop running 6 iterations
+  let foodGuess = prompt ('What is one of my favorite foods?'); //prompting user input
+  if (foodGuess === favFoods[0]){
+    alert ('Yup, I love ' + foodGuess +'!');
+    totalRight = totalRight +1;
+    break;
+  }
+  else if (foodGuess === favFoods[1]){
+    alert ('Yup, I love ' + foodGuess +'!');
+    totalRight = totalRight +1;
+    break;
+  }
+  else if (foodGuess === favFoods[2]){
+    alert ('Yup, I love ' + foodGuess +'!');
+    totalRight = totalRight +1;
+    break;
+  }
+  else if ( i === 5){
+    alert ('Nice try! But cookies, steak, and bacon are my favorite foods!');
+  }
+  else {
+    alert ('Try again!');
+  }
 }
 
-function foodILike (){
-  let answers = ['steak', 'cookies', 'burgers', 'jerky'];
-  let attempt = 0;
-  let rightGuess = false;
-  let answer = prompt('What foods do I like to eat?')
-    if(answers [i] === answer){
-        alert ('You are right, I sure do love ' + answer)
-        rightGuess = true;
-    }
-    else {
-        alert('try again!')
-    }
-
-}
+alert ('Thanks ' + username + ' for checking out my page!, your total number of correct answers was ' + totalRight);
